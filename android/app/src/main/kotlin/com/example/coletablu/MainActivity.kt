c2efdbe4e0a1c46429a974290ad73dcd08f560e1
Larissa <larissa.beatrizzcosta@gmail.com>
2025-08-11 19:18:51 -0300

    feat: :sparkles: routes

---
+++ android/app/src/main/kotlin/com/example/coletablu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coletablu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/routes/routes.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+// ignore: avoid_classes_with_only_static_members
+///named routes
+class Routes {
+  ///create routes
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 250),
+          child: const Placeholder(),
+        );
+      case '/Home':
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 250),
+          child: const Placeholder(),
+        );
+      default:
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 250),
+          child: Scaffold(body: Text('Página não encontrada')),
+        );
+    }
+  }
+}
